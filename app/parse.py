071299ba8fd98dfa0c01e5d3c06d4a38a332c88a
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2026-05-06 23:52:48 +0100

    fixed typing

--- app/parse.py
+++ app/parse.py
@@ -6,8 +6,8 @@ from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
 from selenium.common import TimeoutException, WebDriverException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.ie.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from tqdm import tqdm
